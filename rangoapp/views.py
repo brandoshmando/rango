22beebffaa19d7c1c52836f561388b80ff236990
brandoshmando <brancraft@gmail.com>
2014-11-17 12:58:51 -0700

    Add template dirs variable, add dynamic path with template path variable, add first template

--- rangoapp/views.py
+++ rangoapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("<a href='/rangoapp/about'>About</a>")
-
+  template_var = {'boldmessage':'AHHHHHHHH'}
+  return render(request, 'rangoapp/index.html', template_var)
 def about(request):
   return HttpResponse("<a href='/rangoapp'>Index</a>")
