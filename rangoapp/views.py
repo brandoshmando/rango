d0e937834235d2b50f18b07ea5a50c6de8cd5099
brandoshmando <brancraft@gmail.com>
2014-11-18 16:37:50 -0700

    Add query to retrieve top five rango'd pages. Add logic to iterate through and add each to the list on the index template

--- rangoapp/views.py
+++ rangoapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rangoapp.models import Category
 def index(request):
-  template_var = {'boldmessage':'AHHHHHHHH'}
-  return render(request, 'rangoapp/index.html', template_var)
+  top_categories = Category.objects.order_by('-likes')[:5]
+  return render(request, 'rangoapp/index.html', { 'categories': top_categories} )
 def about(request):
   about_vars = {'boldmessage': 'AHHHHHH'}
   return render(request, 'rangoapp/about.html', about_vars)
