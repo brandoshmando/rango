737ec2ab126be1380f013d2f5473893b7a68a435
brandoshmando <brancraft@gmail.com>
2014-11-20 15:09:39 -0700

    Fixed slight bug in cookie counter

--- rangoapp/views.py
+++ rangoapp/views.py
@@ -20,12 +20,12 @@ def index(request):
   visits = int(request.COOKIES.get('visits', '1'))
   reset_last_visit_time = False
 
-  if 'last_visit' in request:
+  if 'last_visit' in request.COOKIES:
     last_visit = request.COOKIES['last_visit']
     last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
     if (datetime.now() - last_visit_time).days > 0:
-      visits += 1
+      visits = visits + 1
       reset_last_visit_time = True
   else:
     reset_last_visit_time = True
