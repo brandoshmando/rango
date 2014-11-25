bd9dc53d6ae96e61b35e8bbcd062af12da9e1e2f
brandoshmando <brancraft@gmail.com>
2014-11-25 16:00:12 -0500

    Add test to check that slug is equal to slugified name of Category

--- rangoapp/tests/test_models.py
+++ rangoapp/tests/test_models.py
@@ -2,6 +2,8 @@ import unittest
 from django.test import TestCase
 from rangoapp.models import Category
 from rangoapp.tests.factories import CategoryFactory
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
 
 class CategoryTests(TestCase):
 
@@ -23,3 +25,10 @@ class CategoryTests(TestCase):
   def test_category_unicode_equals_name(self):
     category = CategoryFactory.create()
     self.assertEqual(category.__unicode__(), category.name)
+
+  def test_slugify_on_category_save(self):
+    category = CategoryFactory()
+    category.save()
+
+    slugged_name = slugify(category.name)
+    self.assertEqual(category.slug, slugged_name)
