711aea293d8d46ba42e0f96c6c008a0d48080bf9
brandoshmando <brancraft@gmail.com>
2014-11-26 11:18:47 -0500

    Add test to check form is invalid without name field

--- rangoapp/tests/test_forms.py
+++ rangoapp/tests/test_forms.py
@@ -13,3 +13,10 @@ class CategoryFormTests(TestCase):
     data = {'name': category.name, 'likes':category.likes, 'views':category.views}
     form = CategoryForm(data=data)
     self.assertTrue(form.is_valid())
+
+  def test_not_valid_without_name(self):
+    category = CategoryFactory.create()
+    data = {'name': '', 'likes':category.likes, 'views':category.views}
+    form = CategoryForm(data=data)
+
+    self.assertFalse(form.is_valid())
